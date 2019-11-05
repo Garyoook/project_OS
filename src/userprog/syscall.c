4e9a635949319a04a90a1bcfb2ee750ca228b803
jy4917 <jy4917@ic.ac.uk>
2019-11-05 13:45:23 +0000

    Revert "complete function header in syscall.h"
    This reverts commit b72865421b23d33e0a7cf63e4f1b7da95f9fbc08.

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,9 +1,7 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
-// stscall-nr include all functions names
 #include <syscall-nr.h>
 #include <user/syscall.h>
-#include <vaddr.h>
 #include "devices/shutdown.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
@@ -19,10 +17,6 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  // user stack esp
-  if (f->esp > PHYS_BASE) {
-    // fail the handler
-  }
   printf ("system call!\n");
   thread_exit ();
 }
@@ -64,12 +58,7 @@ open(const char *file) {
 
 int
 filesize(int fd) {
-  int bits = 0;
-  while (fd>=1) {
-    bits++;
-    fd /= 2;
-  }
-  return bits/8;
+
 }
 
 int
