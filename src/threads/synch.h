26c1e637739a56cc51c6e536c9d18ab76b1f3211
Wenteng  Zhao <wenteng.zhao18@imperial.ac.uk>
2019-11-05 17:34:24 +0000

    safe user access memory (haven't debugged)

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,6 +22,9 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+
+// userProg
+  struct list_elem lockElem;
   };
 
 void lock_init (struct lock *);
--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -2,13 +2,18 @@
 #include <stdio.h>
 #include <syscall-nr.h>
 #include <user/syscall.h>
+#include "threads/vaddr.h"
 #include "devices/shutdown.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "process.h"
-#include "file.h"
+#include "filesys/file.h"
+#include "pagedir.h"
+#include "threads/synch.h"
 
 static void syscall_handler (struct intr_frame *);
+bool check_esp(void const *esp);
+void release_all_locks(struct thread *t);
 
 void
 syscall_init (void) 
@@ -16,9 +21,42 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+// for user access memory
+bool check_esp(void const *esp) {
+  for (int i = 0; i < 4; i++){
+    if (!(is_user_vaddr(esp + i) &&
+    pagedir_get_page(thread_current()->pagedir, esp + i) &&
+    esp+i != NULL)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void release_all_locks(struct thread *t){
+  struct list_elem *e,*next;
+  e = list_begin (&t->locks);
+
+  while ((next = list_next (e)) != list_end (&t->locks)){
+    struct lock *thisLock = list_entry (e, struct lock, lockElem);
+    lock_release(thisLock);
+
+    e = next;
+  }
+}
+// ---------------------------------
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  // for user access memory
+  if (!check_esp(f->esp)){
+    release_all_locks(thread_current());
+    pagedir_clear_page(f->esp, thread_current()->pagedir);
+
+    exit(-1);
+  }
+  // ---------------------------------
   printf ("system call!\n");
   thread_exit ();
 }
