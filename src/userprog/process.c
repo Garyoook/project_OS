e0a65f3ce35aec0a02fe4dac5150b00169abf17a
Gary_LabPC <yg9418@ic.ac.uk>
2019-11-11 17:53:06 +0000

    --

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -71,6 +71,8 @@ static void check_stack_overflow(int used);
 // use this to pass and push the arguments to the stack:
 static bool argument_passing(void **esp, char *file_name) {
 // push arguments to the stack;
+  enum intr_level old_level;
+  old_level = intr_disable();
   size_t cmdLen = strlen(file_name);
   char s[cmdLen];
   strlcpy(s, file_name, (cmdLen + 1));
@@ -100,8 +102,6 @@ static bool argument_passing(void **esp, char *file_name) {
     check_stack_overflow(bytes_used);
     addrArr[i] = *esp;
   }
-
-  // note that we have got the argc;
 
   // Aligning the esp to a nearest multiple of 4 DID NOT IMPLEMENT#
   void *addr = (void *) ROUND_DOWN ((uint64_t) *esp , 4);
@@ -142,6 +142,8 @@ static bool argument_passing(void **esp, char *file_name) {
   memcpy (*esp, &nullPtr, sizeof (void *));
   bytes_used += sizeof(void *);
   check_stack_overflow(bytes_used);
+
+  intr_set_level(old_level);
 
   return true;
 
