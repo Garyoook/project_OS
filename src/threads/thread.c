526b7ec6609cc76f25efec048e3b5f08896e9557
Gary_LabPC <yg9418@ic.ac.uk>
2019-11-15 15:41:22 +0000

    [absolute final version] remove unused code wriiten earlier

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -644,19 +644,6 @@ allocate_tid (void)
   return tid;
 }
 
-
-struct thread* lookup_tid(tid_t tid){
-  if (!list_empty(&all_list)) {
-    struct list_elem *e = list_begin(&all_list);
-    while (e != list_end(&all_list)){
-      struct thread *t = list_entry(e, struct thread, allelem);
-      if (t->tid == tid) return t;
-      e = e->next;
-    }
-  }
-  return NULL;
-}
-
 /* Offset of `stack' member within `struct thread'.
    Used by switch.S, which can't figure it out on its own. */
 uint32_t thread_stack_ofs = offsetof (struct thread, stack);
