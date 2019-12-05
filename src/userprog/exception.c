f561892dda2b265aaa9536298ea330e038553755
jy4917 <jy4917@ic.ac.uk>
2019-12-05 11:46:02 +0000

    improve functionality of swap

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -225,8 +225,7 @@ page_fault(struct intr_frame *f) {
   } else {
     /* reclamation here:*/
     if (sup_page->evicted) {
-      printf("swap index : %d", swap_index);
-      read_from_swap(upage, (size_t) swap_index);
+      read_from_block(upage, (size_t) sup_page->reclaim_index);
     }
 
     /* stack growing */
--- src/vm/swap.h
+++ src/vm/swap.h
@@ -5,13 +5,8 @@
 #include "stddef.h"
 
 void swap_init();
-void write_to_block(void* frame, int index);
-void read_from_block(void* frame, int index);
-bool write_to_swap(void *kpage);
-bool read_from_swap(void *kpage, size_t start);
-struct bitmap *swap_bmap;
-int swap_index;
-static struct block *swap_block;
+size_t write_to_block(void* page);
+void read_from_block(void* page, size_t index);
 
 #endif //PINTOS_47_SWAP_H
 
